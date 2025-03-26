b67e0dfcec249be168f64f7ca8371c3d82badb5e
Nele90 <65944528+Nele90@users.noreply.github.com>
2025-03-26 20:53:10 +0100

    Update DBA Check Tlog.sql

--- SQL Jobs/Monitoring jobs/DBA Check Tlog.sql
+++ SQL Jobs/Monitoring jobs/DBA Check Tlog.sql
@@ -23,7 +23,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA: Check Tlog',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Database Maintenance]', 
-		@owner_login_name=N'sma\radevic_admin', @job_id = @jobId OUTPUT
+		@owner_login_name=N'owner name', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 /****** Object:  Step [check if some tlog is full than 90%]    Script Date: 3/8/2023 12:47:28 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'check if some tlog is full than 90%', 
