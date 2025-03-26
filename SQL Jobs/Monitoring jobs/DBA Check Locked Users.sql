313d9d1c04376e04db85f4962640555447db788b
Nele90 <65944528+Nele90@users.noreply.github.com>
2025-03-26 20:51:27 +0100

    Update DBA Check Locked Users.sql

--- SQL Jobs/Monitoring jobs/DBA Check Locked Users.sql
+++ SQL Jobs/Monitoring jobs/DBA Check Locked Users.sql
@@ -23,7 +23,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA: Check Locked Users',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Database Maintenance]', 
-		@owner_login_name=N'sma\radevic_admin', @job_id = @jobId OUTPUT
+		@owner_login_name=N'ownername', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 /****** Object:  Step [Check locked users]    Script Date: 3/8/2023 12:46:53 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Check locked users', 
