561653301841a6404ff2acfb78cbe9e9e90e89e7
Nele90 <65944528+Nele90@users.noreply.github.com>
2025-03-26 20:54:07 +0100

    Update DBA Check Wait Stats.sql

--- SQL Jobs/Monitoring jobs/DBA Check Wait Stats.sql
+++ SQL Jobs/Monitoring jobs/DBA Check Wait Stats.sql
@@ -23,7 +23,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA: Check Wait Stats',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Database Maintenance]', 
-		@owner_login_name=N'SMA\radevic_admin', @job_id = @jobId OUTPUT
+		@owner_login_name=N'owner name', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 /****** Object:  Step [Wait Stats]    Script Date: 3/8/2023 12:49:16 PM ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Wait Stats', 
