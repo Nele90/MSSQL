8c5dd1918ceb5fb2cf2a6bebbc0283c4208ed84f
Nele90 <65944528+Nele90@users.noreply.github.com>
2025-03-26 20:46:17 +0100

    Update DBA Sync_Logins.sql

--- SQL Jobs/Maintenance Job/DBA Sync_Logins.sql
+++ SQL Jobs/Maintenance Job/DBA Sync_Logins.sql
@@ -23,7 +23,7 @@ EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'DBA: Sync_Logins',
 		@delete_level=0, 
 		@description=N'No description available.', 
 		@category_name=N'[Uncategorized (Local)]', 
-		@owner_login_name=N'SMA\radevic_admin', @job_id = @jobId OUTPUT
+		@owner_login_name=N'owner name', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 /****** Object:  Step [Sync_Logins]    Script Date: 11.09.2023 09:46:18 ******/
 EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Sync_Logins', 
