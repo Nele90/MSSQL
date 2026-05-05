722e00f594ec36b29fb5224b66b190fe64506ad8
NenadRadevic <65944528+Nele90@users.noreply.github.com>
2026-05-05 13:44:03 +0200

    Remove comment on avg_fragmentation_in_percent check
    Removed comment about IndexOptimize checking avg_fragmentation_in_percent.

--- SQL_scripts/Optimization/View_Fragmentation_All_Indexes_DMV.sql
+++ SQL_scripts/Optimization/View_Fragmentation_All_Indexes_DMV.sql
@@ -6,5 +6,3 @@ FROM
 	sys.dm_db_index_physical_stats(NULL,NULL,NULL,NULL,NULL) ips
 		JOIN
 	sys.indexes i ON ips.OBJECT_ID = i.OBJECT_ID AND ips.index_id = i.index_id
-
-IndexOptimize checks avg_fragmentation_in_percent in sys.dm_db_index_physical_stats to determine the fragmentation.
